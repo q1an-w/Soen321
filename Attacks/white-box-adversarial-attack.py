b1700f386e0d27c2987aa6ea032febb1c0a296b5
Karina Sanchez <karinasd007@gmail.com>
2025-03-13 19:24:35 -0400

    Evaluate models

--- Attacks/white-box-adversarial-attack.py
+++ Attacks/white-box-adversarial-attack.py
@@ -3,9 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
-
+import sys
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from evaluate_performance import evaluatePerformance
 
 # ###########################################################################################
 ## Poisoining attack
@@ -30,8 +32,17 @@ print(feature_importances)
 
 # Modify columns of training data with the most feature importance
 data_copy = data.copy()
-data_copy['ratio_to_median_purchase'] = data_copy['ratio_to_median_purchase'] * 2
+data_copy['ratio_to_median_purchase_price'] = data_copy['ratio_to_median_purchase_price'] * 2
 data_copy['online_order'] = 0
 data_copy['distance_from_home'] = 5
 
+
+new_X = data_copy.drop(columns=['fraud'])
+new_y = data_copy["fraud"].values  # Labels (0 = Not Fraud, 1 = Fraud)
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(new_X, new_y, test_size=0.2, random_state=42, stratify=y)
+poisoned_model = DecisionTreeClassifier(random_state=42)
+poisoned_model.fit(X_train, y_train)
+evaluatePerformance(poisoned_model,X_test,y_test)
 
