261e4228890e053d32cbbec04bef983ae1c62b58
Karina Sanchez <karinasd007@gmail.com>
2025-04-17 20:23:03 -0400

    Updated black-box adversarial attack to properly produce all three output files

--- Attacks/black-box-adversarial-attack.py
+++ Attacks/black-box-adversarial-attack.py
@@ -11,7 +11,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from KaggleModel.use_model import predict_fraud
 from KaggleModel.use_random_forest_model import predict_fraud_v2
-# from KaggleModel.use_gradient_boosting_model import predict_fraud_v3
+from KaggleModel.use_gradient_boosting_model import predict_fraud_v3
 from evaluate_performance import evaluatePerformance
 
 # ##################################################################################################################################
@@ -29,60 +29,34 @@ dt_output = os.path.join(os.path.dirname(__file__), 'output_file.csv')
 rf_output = os.path.join(os.path.dirname(__file__), 'rf_output_file.csv')
 gb_output = os.path.join(os.path.dirname(__file__), 'gb_output_file.csv')
 
-# Decision tree output file --uncomment if you want to generate file again (this file found to have good results through trial and error)
-# with open(dt_output, 'w') as file:
-#     file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
+# ############################################## Decision Tree Model Extraction Attack ##############################################
 
-#     for x in range(10000):
-#         attempt = generate_random_input()
-#         file.write(",".join(map(str, attempt)))
-#         file.write(",")
-#         result = predict_fraud(attempt)
-#         if result == 100.0:
-#             result = 1
-#         else:
-#             result = 0
-#         file.write(str(result))
-#         file.write("\n")
+# Creating decision tree output file
+if not os.path.exists(dt_output):
+    with open(dt_output, 'w') as file:
+        file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
 
-# Random forest output file --uncomment if you want to generate file again (this file found to have good results through trial and error)
-with open(rf_output, 'w') as file:
-    file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
+        for x in range(10000):
+            attempt = generate_random_input()
+            file.write(",".join(map(str, attempt)))
+            file.write(",")
+            result = predict_fraud(attempt)
+            if result == 100.0:
+                result = 1
+            else:
+                result = 0
+            file.write(str(result))
+            file.write("\n")
+else:
+    print("Output file for decision tree exists!")
 
-    for x in range(10000):
-        attempt = generate_random_input()
-        file.write(",".join(map(str, attempt)))
-        file.write(",")
-        result = predict_fraud_v2(attempt)
-        if result == 100.0:
-            result = 1
-        else:
-            result = 0
-        file.write(str(result))
-        file.write("\n")
-
-# Gradient boosting output file --uncomment if you want to generate file again (this file found to have good results through trial and error)
-# with open(gb_output, 'w') as file:
-#     file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
-
-#     for x in range(10000):
-#         attempt = generate_random_input()
-#         file.write(",".join(map(str, attempt)))
-#         file.write(",")
-#         result = predict_fraud_v3(attempt)
-#         if result == 100.0:
-#             result = 1
-#         else:
-#             result = 0
-#         file.write(str(result))
-#         file.write("\n")
-
-# ############################################## Training surrogate model for decision tree ##############################################
+# Reading decision tree output file
 dt_data = pd.read_csv(dt_output)
 dt_X = dt_data.drop(columns=['target'])
 dt_y = dt_data["target"].values
 X_train, X_test, y_train, y_test = train_test_split(dt_X, dt_y, test_size=0.2, random_state=42, stratify=dt_y)
 
+# Training decision tree surrogate
 print("Training decision tree surrogate....")
 dt_model = DecisionTreeClassifier(random_state=42)
 dt_model.fit(X_train, y_train)
@@ -93,12 +67,34 @@ print("Evaluating decision tree surrogate....")
 evaluatePerformance(dt_model,X_test,y_test)
 print("Finished evaluating decision tree surrogate!")
 
-# ############################################## Training surrogate model for random forest ##############################################
+# ############################################## Random Forest Model Extraction Attack ##############################################
+
+# Creating random forest output file
+if not os.path.exists(rf_output):
+    with open(rf_output, 'w') as file:
+        file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
+
+        for x in range(10000):
+            attempt = generate_random_input()
+            file.write(",".join(map(str, attempt)))
+            file.write(",")
+            result = predict_fraud_v2(attempt)
+            if result == 100.0:
+                result = 1
+            else:
+                result = 0
+            file.write(str(result))
+            file.write("\n")
+else:
+    print("Output file for random forest exists!")
+
+# Reading random forest output file
 rf_data = pd.read_csv(rf_output)
 rf_X = rf_data.drop(columns=['target'])
 rf_y = rf_data["target"].values
 X_train, X_test, y_train, y_test = train_test_split(rf_X, rf_y, test_size=0.2, random_state=42, stratify=rf_y)
 
+# Training random forest surrogate
 print("Training random forest surrogate....")
 rf_model = RandomForestClassifier(random_state=42)
 rf_model.fit(X_train, y_train)
@@ -109,18 +105,40 @@ print("Evaluating random forest surrogate....")
 evaluatePerformance(rf_model,X_test,y_test)
 print("Finished evaluating random forest surrogate!")
 
-# ############################################## Training surrogate model for gradient booting ##############################################
-# gb_data = pd.read_csv(gb_output)
-# gb_X = gb_data.drop(columns=['target'])
-# gb_y = gb_data["target"].values
-# X_train, X_test, y_train, y_test = train_test_split(gb_X, gb_y, test_size=0.2, random_state=42, stratify=gb_y)
+# ############################################## Gradient Boosting Model Extraction Attack ##############################################
 
-# print("Training gradient booster surrogate....")
-# rf_model = GradientBoostingClassifier(random_state=42)
-# rf_model.fit(X_train, y_train)
-# print("Finished training gradient booster surrogate!")
+# Creating gradient boosting output file
+if not os.path.exists(gb_output):
+    with open(gb_output, 'w') as file:
+        file.write('feature1,feature2,feature3,feature4,feature5,feature6,feature7,target\n')
 
-# # Evaluating gradient booster
-# print("Evaluating gradient booster surrogate....")
-# evaluatePerformance(rf_model,X_test,y_test)
-# print("Finished evaluating gradient booster surrogate!")
+        for x in range(10000):
+            attempt = generate_random_input()
+            file.write(",".join(map(str, attempt)))
+            file.write(",")
+            result = predict_fraud_v3(attempt)
+            if result > 95.0: # Done as greater than 95% and not 100% because this model predicts with at most a probability of 98% (usually)
+                result = 1
+            else:
+                result = 0
+            file.write(str(result))
+            file.write("\n")
+else:
+    print("Output file for gradient boosting exists!")
+
+# Reading gradient boosting output file
+gb_data = pd.read_csv(gb_output)
+gb_X = gb_data.drop(columns=['target'])
+gb_y = gb_data["target"].values
+X_train, X_test, y_train, y_test = train_test_split(gb_X, gb_y, test_size=0.2, random_state=42, stratify=gb_y)
+
+# Training gradient boosting surrogate
+print("Training gradient booster surrogate....")
+rf_model = GradientBoostingClassifier(random_state=42)
+rf_model.fit(X_train, y_train)
+print("Finished training gradient booster surrogate!")
+
+# Evaluating gradient booster
+print("Evaluating gradient booster surrogate....")
+evaluatePerformance(rf_model,X_test,y_test)
+print("Finished evaluating gradient booster surrogate!")
