b40b86c16b195fbf7ac51e7528f1089b7d2eeb10
Karina Sanchez <94981080+KarinaSandur@users.noreply.github.com>
2025-04-21 13:13:07 -0400

    Update KaggleModel/fix_gb_model.py

--- KaggleModel/fix_gb_model.py
+++ KaggleModel/fix_gb_model.py
@@ -1,6 +1,6 @@
 import joblib
 
-# Step 1: Load the existing model that gives the _loss error
+# Load existing model that gives the _loss error
 old_model = joblib.load("fraud_model_gb.pkl")
 
 # Step 2: Save it again in a clean format with no legacy metadata
